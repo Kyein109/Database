68d0b61acc98403360d4871c6d2681051c581841
Aaron Tran <s3540620@student.rmit.edu.au>
2018-04-01 16:57:43 +1000

    asd

--- Database/src/dbload.java
+++ Database/src/dbload.java
@@ -1,8 +1,12 @@
 import java.awt.List;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
@@ -11,62 +15,42 @@ public class dbload {
 
 	public static void main(String[] args) 
 	{
-		String fileLocation = "E:\\Documents\\Database\\FormattedCSV.csv";
+		String fileLocation = "E:\\Documents\\Database\\New Text Document.txt";
         String outputLocation = "E:\\Documents\\Database\\heap.pagesize";
-
+          
         File file = new File(fileLocation);
-        ArrayList<Byte> changed = new ArrayList<Byte>();
-        
+        String regname = null;
         try 
         {
         	BufferedReader br = new BufferedReader(new FileReader(file));
             String line;
+            
+            FileOutputStream outfile = new FileOutputStream("E:\\Documents\\Database\\heap.pagesize");  
+            DataOutputStream data = new DataOutputStream(outfile);
+            
             StringTokenizer strtok;
-            String regname = null;
-            String bname;
-            String status;
-            String regDT;
-            String cancelDT;
-            String renewDT;
-            String stateNo;
-            String stateofreg;
-            String ABN;
             
             while((line=br.readLine()) != null) 
             {
             	strtok = new StringTokenizer(line, ",");
-            	
+                
             	while(strtok.hasMoreTokens())
             	{
             		regname = strtok.nextToken();
-            		bname = strtok.nextToken();
-            		status = strtok.nextToken();
-            		regDT = strtok.nextToken();
-            		cancelDT = strtok.nextToken();
-            		renewDT = strtok.nextToken();
-            		stateNo = strtok.nextToken();
-            		stateofreg = strtok.nextToken();
-            		ABN = strtok.nextToken();
+            		
+                	byte[] b = regname.getBytes();
+                	
+                	for (int i = 0; i < b.length; i++) 
+                	{
+                		data.writeInt(b[i]);
+                    }
             	}
-
-        		byte[] byt = regname.getBytes();
-        		for (byte b : byt) {
-        			System.out.println(Arrays.toString(byt));
-        		}
             }
+            data.flush();  
+            data.close();  
             System.out.println("done");
             //close all files
             br.close();
-            
-           //write all the lines to the file
-           File out = new File(outputLocation);
-           PrintWriter write = new PrintWriter(out);
-           for(int i = 0 ; i < changed.size() ; i++) 
-           {
-        	   write.println(changed.get(i));
-           }
-
-           write.close();
         }
         catch(Exception ex) 
         {
