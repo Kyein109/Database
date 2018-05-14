0a7425d3c2cfe04b73344229e905152a601535be
Aaron Tran <s3540620@student.rmit.edu.au>
2018-05-15 03:10:21 +1000

    basic file reading

--- Database/src/hashload.java
+++ Database/src/hashload.java
@@ -1,4 +1,47 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.util.Hashtable;
+import java.util.StringTokenizer;
 
 public class hashload {
-
+	
+	public static void main(String[] args) throws FileNotFoundException 
+	{
+		String pagesize = args[0];
+		
+		String fileLocation = "./heap.4096";
+		String outputLocation = "./hash." + pagesize;
+		File file = new File(fileLocation);
+		String line;
+		StringTokenizer strtok;
+		
+		Hashtable< Integer, String > hash = new Hashtable< Integer, String >();
+       
+		try 
+        {
+			BufferedReader heapfile = new BufferedReader(new FileReader(file));
+	        FileOutputStream outfile = new FileOutputStream(outputLocation);  
+	        DataOutputStream data = new DataOutputStream(outfile);
+	        
+	        while((line=heapfile.readLine()) != null) 
+            {
+	        	strtok = new StringTokenizer(line, ",");
+	        	
+	        	
+	        	
+            }
+	        data.flush();  
+            data.close();
+        }
+		catch(Exception ex) 
+        {
+            System.err.println("Error");
+        }
+    
+        
+	}
 }
